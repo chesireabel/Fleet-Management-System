f37b5e43acf78ef922996852b39bcba8bde70db4
ABEL CHESIRE <chesireabel1@gmail.com>
2025-01-24 02:34:56 +0300

    New Changes

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
@@ -6,10 +7,11 @@ const PORT =  3000;
 
 //MIDDLEWARE
 app.use(express.json());
+app.use(cors());
 
 //ROUTES
 app.get('/',(req,res) => {
-    res.send('Lets get started!');
+    res.json('Lets get started!');
 });
 
 app.listen(PORT, () => {
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import LoginPage from './pages/LoginPage';
 function App() {
-
   return (
    <>
-  Lets start!
+  <Router>
+    <Routes>
+      <Route path="/"  element = {<LoginPage/>}></Route>
+
+    </Routes>
+    
+  </Router>
    </>
   )
 }
